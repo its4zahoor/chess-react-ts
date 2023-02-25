92b9768b5aac1a5e4d80cd09c22cb717bc95664f
its4zahoor <its4zahoor@gmail.com>
2023-02-25 15:16:10 +0500

    adds basic chess board layout and styling
    Signed-off-by: its4zahoor <its4zahoor@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,27 @@
+import "./App.css";
+
+const FILES = ["a", "b", "c", "d", "e", "f", "g", "h"];
+
 function App() {
-  return <div className="">New Clean React + Vite + Typescript</div>;
+  const board = Array(8)
+    .fill(0)
+    .map(() => Array(8).fill(0));
+
+  const getBg = (i: number, j: number) =>
+    (i + j) % 2 !== 0 ? "text-bg-success" : "bg-primary-subtle text-success";
+
+  return (
+    <div className="chess-grid">
+      {board.map((rank, i) =>
+        rank.map((_, j) => (
+          <div key={j} className={getBg(i, j)}>
+            {j === 0 && <span className="cell rank">{8 - i}</span>}
+            {i === 7 && <span className="cell file">{FILES[j]}</span>}
+          </div>
+        ))
+      )}
+    </div>
+  );
 }
 
 export default App;
