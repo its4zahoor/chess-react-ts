494a04fa4bdc2e522a74588f774dfd710ff25105
its4zahoor <its4zahoor@gmail.com>
2023-02-26 22:07:13 +0500

    adds flip board, FEN parsing for starting postion
    Signed-off-by: its4zahoor <its4zahoor@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,43 @@
+import { useState } from "react";
 import "./App.css";
 
 const FILES = ["a", "b", "c", "d", "e", "f", "g", "h"];
+const BLACK = ["p", "r", "b", "n", "k", "q"];
+const WHITE = ["P", "R", "B", "N", "K", "Q"];
+
+const FEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR";
+
+const BOARD = FEN.split("/").map((file) =>
+  file.split("").flatMap((piece) => {
+    if (BLACK.includes(piece)) return piece;
+    if (WHITE.includes(piece)) return piece;
+    return Array(Number(file)).fill("");
+  })
+);
 
 function App() {
-  const board = Array(8)
-    .fill(0)
-    .map(() => Array(8).fill(0));
+  const [board, setBoard] = useState(BOARD);
+  const [isFlipped, setFlipped] = useState(false);
 
   const getBg = (i: number, j: number) =>
     (i + j) % 2 !== 0 ? "text-bg-success" : "bg-primary-subtle text-success";
 
+  const getRank = (n: number) => (isFlipped ? n + 1 : 8 - n);
+  const getFile = (n: number) => (isFlipped ? 7 - n : n);
+
   return (
     <div className="chess-grid">
-      {board.map((rank, i) =>
-        rank.map((_, j) => (
-          <div key={j} className={getBg(i, j)}>
-            {j === 0 && <span className="cell rank">{8 - i}</span>}
-            {i === 7 && <span className="cell file">{FILES[j]}</span>}
+      {board.map((row, rank) =>
+        row.map((piece, file) => (
+          <div key={`${rank}${file}`} className={`cell ${getBg(rank, file)}`}>
+            {file === 0 && <span className="rank">{getRank(rank)}</span>}
+            {rank === 7 && <span className="file">{FILES[getFile(file)]}</span>}
+            {piece}
           </div>
         ))
       )}
+
+      <button onClick={() => setFlipped((f) => !f)}>flip</button>
     </div>
   );
 }
