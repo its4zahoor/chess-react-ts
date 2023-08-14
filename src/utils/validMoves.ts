c8d511cb9b69794d1a1bf5b83703e70f8deb0211
its4zahoor <its4zahoor@gmail.com>
2023-08-14 18:15:24 +0500

    refactor for loops to map and flatMap
    Signed-off-by: its4zahoor <its4zahoor@gmail.com>

--- src/utils/validMoves.ts
+++ src/utils/validMoves.ts
@@ -10,22 +10,11 @@ type movesT = Array<Array<number>>
 const shiftBy2 = (v: number) => [v - 2, v + 2].filter(validIndex)
 const shiftBy1 = (v: number) => [v - 1, v + 1].filter(validIndex)
 
-export const knightMoves = (rank: number, file: number) => {
-  const LShapes = [
+export const knightMoves = (rank: number, file: number): movesT => {
+  const slices = [
     [shiftBy1(rank), shiftBy2(file)],
     [shiftBy2(rank), shiftBy1(file)],
   ]
 
-  const moves: movesT = []
-
-  LShapes.forEach(a => {
-    const [R, F] = a
-    for (let i = 0; i < R.length; i++) {
-      for (let j = 0; j < F.length; j++) {
-        moves.push([R[i], F[j]])
-      }
-    }
-  })
-
-  return moves
+  return slices.flatMap(([R, F]) => R.flatMap(r => F.map(f => [r, f])))
 }
