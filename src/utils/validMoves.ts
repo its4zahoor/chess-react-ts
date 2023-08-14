c399752d16d24434d5550ada02f3b3339c0839b5
its4zahoor <its4zahoor@gmail.com>
2023-08-14 20:57:36 +0500

    rook and queen logic complete
    Signed-off-by: its4zahoor <its4zahoor@gmail.com>

--- src/utils/validMoves.ts
+++ src/utils/validMoves.ts
@@ -1,7 +1,6 @@
 export const validIndex = (i: number) => i >= 0 && i <= 7
 
-export const pawnMoves = (rank: number, file: number) => {
-  console.log('Zahoor: ~ file: validMoves.ts:2 ~ rank, file:', rank, file)
+export const pawn = (rank: number, file: number) => {
   return [rank, file]
 }
 
@@ -10,7 +9,7 @@ type movesT = Array<Array<number>>
 const shiftBy2 = (v: number) => [v - 2, v + 2].filter(validIndex)
 const shiftBy1 = (v: number) => [v - 1, v + 1].filter(validIndex)
 
-export const knightMoves = (rank: number, file: number): movesT => {
+export const knight = (rank: number, file: number): movesT => {
   const slices = [
     [shiftBy1(rank), shiftBy2(file)],
     [shiftBy2(rank), shiftBy1(file)],
@@ -26,7 +25,7 @@ const subN = (x: number, y: number) => (n: number) => [x - n, y - n]
 const addNsubN = (x: number, y: number) => (n: number) => [x + n, y - n]
 const subNaddN = (x: number, y: number) => (n: number) => [x - n, y + n]
 
-export const bishopMoves = (rank: number, file: number): movesT => {
+export const bishop = (rank: number, file: number): movesT => {
   const moves = [
     indices.map(x => addN(rank, file)(x)).filter(x => x.every(validIndex)),
     indices.map(x => subN(rank, file)(x)).filter(x => x.every(validIndex)),
@@ -34,5 +33,20 @@ export const bishopMoves = (rank: number, file: number): movesT => {
     indices.map(x => subNaddN(rank, file)(x)).filter(x => x.every(validIndex)),
   ]
 
+  return moves.flatMap(x => x)
+}
+
+export const rook = (rank: number, file: number): movesT => {
+  const moves = [
+    indices.map(x => addN(rank, file - x)(x)).filter(x => x.every(validIndex)),
+    indices.map(x => subN(rank, file + x)(x)).filter(x => x.every(validIndex)),
+    indices.map(x => addN(rank - x, file)(x)).filter(x => x.every(validIndex)),
+    indices.map(x => subN(rank + x, file)(x)).filter(x => x.every(validIndex)),
+  ]
+  return moves.flatMap(x => x)
+}
+
+export const queen = (rank: number, file: number): movesT => {
+  const moves = [bishop(rank, file), rook(rank, file)]
   return moves.flatMap(x => x)
 }
